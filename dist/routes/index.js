4393313c0721a7119ff1338f09d3f8e4af94d523
Facundo Martin Gordillo <me@facundogordillo.com>
2023-02-17 23:18:29 +0100

    fix: :broom: removed unused files

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/* GET home page. */
-router.get("/", function (req, res, next) {
-    res.render("index", { title: "Express" });
-});
-module.exports = router;
-//# sourceMappingURL=index.js.map
